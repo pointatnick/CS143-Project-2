a950dd8746f2102a88aefb10bcf87b511b0bcd8a
Nick Ma <nhma@Raven.local>
2016-05-31 15:47:56 -0700

    compiled

--- sql/core/src/main/scala/org/apache/spark/sql/execution/DiskHashedRelation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/DiskHashedRelation.scala
@@ -11,8 +11,8 @@ import org.apache.spark.sql.execution.CS143Utils._
 import scala.collection.JavaConverters._
 
 /**
- * This trait represents a regular relation that is hash partitioned and spilled to
- * disk.
+ * This trait represents a regular relation
+ * that is hash partitioned and spilled to disk.
  */
 private[sql] sealed trait DiskHashedRelation {
   /**
@@ -22,7 +22,8 @@ private[sql] sealed trait DiskHashedRelation {
   def getIterator(): Iterator[DiskPartition]
 
   /**
-   * Close all the partitions for this relation. This should involve deleting the files hashed into.
+   * Close all the partitions for this relation.
+   * This should involve deleting the files hashed into.
    */
   def closeAllPartitions()
 }
@@ -32,8 +33,9 @@ private[sql] sealed trait DiskHashedRelation {
  *
  * @param partitions the disk partitions that we are going to spill to
  */
-protected [sql] final class GeneralDiskHashedRelation(partitions: Array[DiskPartition])
-    extends DiskHashedRelation with Serializable {
+protected [sql] final
+class GeneralDiskHashedRelation(partitions: Array[DiskPartition])
+  extends DiskHashedRelation with Serializable {
 
   override def getIterator() = {
     // IMPLEMENT ME
@@ -45,9 +47,7 @@ protected [sql] final class GeneralDiskHashedRelation(partitions: Array[DiskPart
   }
 }
 
-private[sql] class DiskPartition (
-                                  filename: String,
-                                  blockSize: Int) {
+private[sql] class DiskPartition (filename: String, blockSize: Int) {
   private val path: Path = Files.createTempFile("", filename)
   private val data: JavaArrayList[Row] = new JavaArrayList[Row]
   private val outStream: OutputStream = Files.newOutputStream(path)
@@ -57,8 +57,9 @@ private[sql] class DiskPartition (
   private var inputClosed: Boolean = false
 
   /**
-   * This method inserts a new row into this particular partition. If the size of the partition
-   * exceeds the blockSize, the partition is spilled to disk.
+   * This method inserts a new row into this particular partition.
+   * If the size of the partition exceeds the blockSize,
+   * the partition is spilled to disk.
    *
    * @param row the [[Row]] we are adding
    */
@@ -67,7 +68,8 @@ private[sql] class DiskPartition (
   }
 
   /**
-   * This method converts the data to a byte array and returns the size of the byte array
+   * This method converts the data to a byte array and
+   * returns the size of the byte array
    * as an estimation of the size of the partition.
    *
    * @return the estimated size of the data
@@ -82,7 +84,8 @@ private[sql] class DiskPartition (
   private[this] def spillPartitionToDisk() = {
     val bytes: Array[Byte] = getBytesFromList(data)
 
-    // This array list stores the sizes of chunks written in order to read them back correctly.
+    // This array list stores the sizes of chunks written
+    // in order to read them back correctly.
     chunkSizes.add(bytes.size)
 
     Files.write(path, bytes, StandardOpenOption.APPEND)
@@ -90,14 +93,16 @@ private[sql] class DiskPartition (
   }
 
   /**
-   * If this partition has been closed, this method returns an Iterator of all the
-   * data that was written to disk by this partition.
+   * If this partition has been closed, this method returns
+   * an Iterator of all the data that was written to disk by this partition.
    *
    * @return the [[Iterator]] of the data
    */
   def getData(): Iterator[Row] = {
     if (!inputClosed) {
-      throw new SparkException("Should not be reading from file before closing input. Bad things will happen!")
+      throw new SparkException(
+        "Should not be reading from file before closing input.
+        Bad things will happen!")
     }
 
     new Iterator[Row] {
@@ -116,8 +121,8 @@ private[sql] class DiskPartition (
       }
 
       /**
-       * Fetches the next chunk of the file and updates the iterator. Should return true
-       * unless the iterator is empty.
+       * Fetches the next chunk of the file and updates the iterator.
+       * Should return true unless the iterator is empty.
        *
        * @return true unless the iterator is empty.
        */
@@ -129,11 +134,12 @@ private[sql] class DiskPartition (
   }
 
   /**
-   * Closes this partition, implying that no more data will be written to this partition. If getData()
-   * is called without closing the partition, an error will be thrown.
+   * Closes this partition, implying that no more data will be written
+   * to this partition. If getData() is called without closing the partition,
+   * an error will be thrown.
    *
-   * If any data has not been written to disk yet, it should be written. The output stream should
-   * also be closed.
+   * If any data has not been written to disk yet, it should be written.
+   * The output stream should also be closed.
    */
   def closeInput() = {
     // IMPLEMENT ME
@@ -142,7 +148,8 @@ private[sql] class DiskPartition (
 
 
   /**
-   * Closes this partition. This closes the input stream and deletes the file backing the partition.
+   * Closes this partition. This closes the input stream and
+   * deletes the file backing the partition.
    */
   private[sql] def closePartition() = {
     inStream.close()
@@ -153,26 +160,23 @@ private[sql] class DiskPartition (
 private[sql] object DiskHashedRelation {
 
   /**
-   * Given an input iterator, partitions each row into one of a number of [[DiskPartition]]s
-   * and constructors a [[DiskHashedRelation]].
+   * Given an input iterator, partitions each row into one of a number of
+   * [[DiskPartition]]s and constructors a [[DiskHashedRelation]].
    *
-   * This executes the first phase of external hashing -- using a course-grained hash function
-   * to partition the tuples to disk.
+   * This executes the first phase of external hashing
+   * -- using a course-grained hash function to partition the tuples to disk.
    *
-   * The block size is approximately set to 64k because that is a good estimate of the average
-   * buffer page.
+   * The block size is approximately set to 64k
+   * because that is a good estimate of the average buffer page.
    *
    * @param input the input [[Iterator]] of [[Row]]s
-   * @param keyGenerator a [[Projection]] that generates the keys for the input
+   * @param keyGenerator a [[Projection]] that generates keys for the input
    * @param size the number of [[DiskPartition]]s
    * @param blockSize the threshold at which each partition will spill
    * @return the constructed [[DiskHashedRelation]]
    */
-  def apply (
-                input: Iterator[Row],
-                keyGenerator: Projection,
-                size: Int = 64,
-                blockSize: Int = 64000) = {
+  def apply (input: Iterator[Row], keyGenerator: Projection,
+             size: Int = 64, blockSize: Int = 64000) = {
     // IMPLEMENT ME
     null
   }
